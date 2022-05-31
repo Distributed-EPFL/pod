a4f39d47ce2b3a349ef95ce85913a39aeb12f480
Matteo Monti <matteo.monti@epfl.ch>
2022-05-31 13:54:24 +0200

    Test `Passepartout` persistence
    Co-authored-by: Manuel Vidigueira <mvidigueira@users.noreply.github.com>

--- src/passepartout/passepartout.rs
+++ src/passepartout/passepartout.rs
@@ -20,10 +20,54 @@ impl Passepartout {
     }
 
     pub fn load(path: &str) -> Passepartout {
-        bincode::deserialize::<Passepartout>(fs::read(path).unwrap().as_slice()).unwrap()
+        bincode::deserialize(fs::read(path).unwrap().as_slice()).unwrap()
+    }
+
+    pub fn keychain(&self, identity: Identity) -> KeyChain {
+        self.keychains.get(&identity).unwrap().clone()
     }
 
     pub fn save(&self, path: &str) {
         fs::write(path, bincode::serialize(&self).unwrap().as_slice()).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use talk::crypto::Statement;
+
+    #[derive(Serialize)]
+    struct TestHeader;
+
+    #[derive(Serialize)]
+    struct TestStatement(u64);
+
+    impl Statement for TestStatement {
+        type Header = TestHeader;
+        const HEADER: Self::Header = TestHeader;
+    }
+
+    #[test]
+    fn persist() {
+        let original = Passepartout::random(1000);
+        original.save("assets/passepartout.bin");
+
+        let message = TestStatement(42);
+
+        let signatures = original
+            .keychains
+            .iter()
+            .map(|(identity, keychain)| (*identity, keychain.sign(&message).unwrap()))
+            .collect::<HashMap<_, _>>();
+
+        let loaded = Passepartout::load("assets/passepartout.bin");
+
+        for (identity, signature) in signatures {
+            signature
+                .verify(&loaded.keychain(identity).keycard(), &message)
+                .unwrap();
+        }
     }
 }
