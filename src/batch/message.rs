28361ce4059d538c9bc5f56b3d6e61202bd0278f
Matteo Monti <matteo.monti@epfl.ch>
2022-06-02 12:27:30 +0200

    Scaffold `Message`

---
+++ src/batch/message.rs
@@ -0,0 +1,1 @@
+pub type Message = [u8; 8];
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+mod batch;
 mod directory;
 mod membership;
 mod passepartout;
 
+pub use batch::Message;
 pub use directory::Directory;
 pub use membership::{Certificate, CertificateError, Membership};
 pub use passepartout::Passepartout;
