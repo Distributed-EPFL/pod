50a1452fb0902c6bb6e10a6243b44e6fb1e4c401
Matteo Monti <matteo.monti@epfl.ch>
2022-06-02 13:44:04 +0200

    Derive `Eq` for `Payload`

--- src/batch/payload.rs
+++ src/batch/payload.rs
@@ -2,7 +2,7 @@ use crate::batch::Message;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Payload {
     pub id: u64,
     pub message: Message,
