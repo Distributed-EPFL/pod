6ec72e44281e61c8def87f4a129bb1f8cca2c23c
Matteo Monti <matteo.monti@epfl.ch>
2022-06-14 15:12:56 +0200

    Fix `Broadcast` trait

--- src/broadcast/broadcast.rs
+++ src/broadcast/broadcast.rs
@@ -2,6 +2,6 @@ use async_trait::async_trait;
 
 #[async_trait]
 pub trait Broadcast {
-    async fn order(payload: &[u8]);
-    async fn deliver() -> Vec<u8>;
+    async fn order(&self, payload: &[u8]);
+    async fn deliver(&self) -> Vec<u8>;
 }
