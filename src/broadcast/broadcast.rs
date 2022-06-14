6dc2c03324f44f9bd54ad021f6bed829bf2a2728
Matteo Monti <matteo.monti@epfl.ch>
2022-06-14 15:13:50 +0200

    Extend `Broadcast` trait

--- src/broadcast/broadcast.rs
+++ src/broadcast/broadcast.rs
@@ -1,7 +1,7 @@
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait Broadcast {
+pub trait Broadcast: 'static + Send + Sync {
     async fn order(&self, payload: &[u8]);
     async fn deliver(&self) -> Vec<u8>;
 }
