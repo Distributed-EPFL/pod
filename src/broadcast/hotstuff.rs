1cc39be9f71e154d04701517efad5e3aaed2e8ea
Matteo Monti <matteo.monti@epfl.ch>
2022-06-14 17:01:04 +0200

    Tidy up implementation of `HotStuff`

--- src/broadcast/hotstuff.rs
+++ src/broadcast/hotstuff.rs
@@ -4,20 +4,20 @@ use crate::broadcast::Broadcast;
 
 use sha1::{Digest, Sha1};
 
-use std::env;
-use std::error::Error;
-use std::net::SocketAddr;
-use std::time::Duration;
+use std::{error::Error, net::SocketAddr};
 
-use tokio::io;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
-use tokio::sync::Mutex;
-use tokio::task;
-use tokio::time;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::Mutex,
+};
 
 pub struct HotStuffInterface {
     stream: TcpStream,
+}
+
+pub struct HotStuff {
+    state: Mutex<HotStuffInterface>,
 }
 
 impl HotStuffInterface {
@@ -37,29 +37,33 @@ impl HotStuffInterface {
 
         let hash = hasher.finalize();
 
-        self.stream.write(&magic.to_le_bytes()).await;
-        self.stream.write(&opcode.to_le_bytes()).await;
-        self.stream.write(&length.to_le_bytes()).await;
-        self.stream.write(&hash[0..4]).await;
-        self.stream.write(payload).await;
+        self.stream.write(&magic.to_le_bytes()).await.unwrap();
+        self.stream.write(&opcode.to_le_bytes()).await.unwrap();
+        self.stream.write(&length.to_le_bytes()).await.unwrap();
+        self.stream.write(&hash[0..4]).await.unwrap();
+        self.stream.write(payload).await.unwrap();
     }
 
     pub async fn deliver(&mut self) -> Vec<u8> {
         let mut buf = vec![0; 13];
 
-        self.stream.read_exact(&mut buf).await;
+        self.stream.read_exact(&mut buf).await.unwrap();
 
         let length = u32::from_le_bytes(buf[5..9].try_into().unwrap());
         let mut msg = vec![0; length.try_into().unwrap()];
 
-        self.stream.read_exact(&mut msg).await;
+        self.stream.read_exact(&mut msg).await.unwrap();
 
         return msg;
     }
 }
 
-pub struct HotStuff {
-    state: Mutex<HotStuffInterface>,
+impl HotStuff {
+    pub async fn connect(addr: &SocketAddr) -> Result<Self, Box<dyn Error>> {
+        Ok(Self {
+            state: Mutex::new(HotStuffInterface::connect(addr).await?),
+        })
+    }
 }
 
 #[async_trait]
@@ -72,13 +76,5 @@ impl Broadcast for HotStuff {
     async fn deliver(&self) -> Vec<u8> {
         let mut iface = self.state.lock().await;
         iface.deliver().await
-    }
-}
-
-impl HotStuff {
-    async fn connect(addr: &SocketAddr) -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            state: Mutex::new(HotStuffInterface::connect(addr).await?),
-        })
     }
 }
