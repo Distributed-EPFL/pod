a8464c9c1ebbe957e62b73c5d07d044dab067fd2
Matteo Monti <matteo.monti@epfl.ch>
2022-06-15 15:54:35 +0200

    Fix `Mutex`es for `HotStuff`

--- src/broadcast/hotstuff.rs
+++ src/broadcast/hotstuff.rs
@@ -8,27 +8,35 @@ use std::{error::Error, net::SocketAddr};
 
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpStream,
+    },
     sync::Mutex,
 };
 
-pub struct HotStuffInterface {
-    stream: TcpStream,
-}
-
 pub struct HotStuff {
-    state: Mutex<HotStuffInterface>,
+    read: Mutex<OwnedReadHalf>,
+    write: Mutex<OwnedWriteHalf>,
 }
 
-impl HotStuffInterface {
-    async fn connect(addr: &SocketAddr) -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            stream: TcpStream::connect(addr).await?,
-        })
-    }
+impl HotStuff {
+    pub async fn connect(addr: &SocketAddr) -> Result<Self, Box<dyn Error>> {
+        let stream = TcpStream::connect(addr).await?;
+        let (read, write) = stream.into_split();
 
-    pub async fn order(&mut self, payload: &[u8]) {
-        println!("In underlying order..");
+        let read = Mutex::new(read);
+        let write = Mutex::new(write);
+
+        Ok(HotStuff { read, write })
+    }
+}
+
+#[async_trait]
+impl Broadcast for HotStuff {
+    async fn order(&self, payload: &[u8]) {
+        let mut write = self.write.lock().await;
+
         let magic: u32 = 0;
         let opcode: u8 = 100;
         let length: u32 = payload.len().try_into().unwrap();
@@ -38,48 +46,25 @@ impl HotStuffInterface {
 
         let hash = hasher.finalize();
 
-        println!("Writing stuff in stream..");
-
-        self.stream.write(&magic.to_le_bytes()).await.unwrap();
-        self.stream.write(&opcode.to_le_bytes()).await.unwrap();
-        self.stream.write(&length.to_le_bytes()).await.unwrap();
-        self.stream.write(&hash[0..4]).await.unwrap();
-        self.stream.write(payload).await.unwrap();
+        write.write(&magic.to_le_bytes()).await.unwrap();
+        write.write(&opcode.to_le_bytes()).await.unwrap();
+        write.write(&length.to_le_bytes()).await.unwrap();
+        write.write(&hash[0..4]).await.unwrap();
+        write.write(payload).await.unwrap();
     }
 
-    pub async fn deliver(&mut self) -> Vec<u8> {
+    async fn deliver(&self) -> Vec<u8> {
+        let mut read = self.read.lock().await;
+
         let mut buf = vec![0; 13];
 
-        self.stream.read_exact(&mut buf).await.unwrap();
+        read.read_exact(&mut buf).await.unwrap();
 
         let length = u32::from_le_bytes(buf[5..9].try_into().unwrap());
         let mut msg = vec![0; length.try_into().unwrap()];
 
-        self.stream.read_exact(&mut msg).await.unwrap();
+        read.read_exact(&mut msg).await.unwrap();
 
         return msg;
-    }
-}
-
-impl HotStuff {
-    pub async fn connect(addr: &SocketAddr) -> Result<Self, Box<dyn Error>> {
-        Ok(Self {
-            state: Mutex::new(HotStuffInterface::connect(addr).await?),
-        })
-    }
-}
-
-#[async_trait]
-impl Broadcast for HotStuff {
-    async fn order(&self, payload: &[u8]) {
-        println!("Locking..");
-        let mut iface = self.state.lock().await;
-        println!("Calling underlying order..");
-        iface.order(payload).await;
-    }
-
-    async fn deliver(&self) -> Vec<u8> {
-        let mut iface = self.state.lock().await;
-        iface.deliver().await
     }
 }
